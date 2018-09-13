d2e6f2a88c1a7d66d0c55c08ce7366622d1206b2
Vivek Rai <mail@raivivek.in>
2018-09-13 12:08:05 -0400

    Build: Thu Sep 13 12:08:05 EDT 2018

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,7 @@ bundle exec jekyll build
 
 ## Add and commit build changes
 git add -A
-git commit -m "Build: `date`"
 
 ## git subtree deployment to gh-pages here
 ## basically, just pushes the `_site` directory to the specified <remote>/<branch>
-[ -n "$(git status --porcelain)" ] && git subtree push --prefix _site origin gh-pages
+git commit -m "Build: `date`" && git subtree push --prefix _site origin gh-pages
