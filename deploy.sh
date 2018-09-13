94cfa13e042a4883bbcfda46c95984242b3157c7
Vivek Rai <mail@raivivek.in>
2018-09-13 12:18:21 -0400

    Add exceptions to _config.yaml; tweak deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ BRANCH=gh-pages
 bundle exec jekyll build
 
 ## Add and commit build changes
-git add -A
-git commit -m "Build: `date`"
+#git add -A
+#git commit -m "Build: `date`"
 
 ## git subtree deployment to gh-pages here
 ## basically, just pushes the `_site` directory to the specified <remote>/<branch>
