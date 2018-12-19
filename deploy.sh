6070ef7a4cacc3055e2ed1d25d8810e4fc93000d
Vivek Rai <mail@raivivek.in>
2018-12-19 14:46:35 -0500

    Fix deploy script
    Signed-off-by: Vivek Rai <mail@raivivek.in>

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,28 @@
 
 ## edit if needed; where'e the [remote] for repository
 REMOTE=origin
-BRANCH=gh-pages
+
+echo "The script will build the website and deploy it to GitHub"
+echo "Make sure all your changes are committed."
+
+read -n 1 -s -r -p "Press any key to continue"
 
 ## Build: requires `bundler`
+echo "\n1. Building website.."
 bundle exec jekyll build
 
-## Add and commit build changes
-#git add -A
-#git commit -m "Build: `date`"
+echo "\n2. Commiting build.."
 
+## Add and commit build changes
+git add -A
+git commit -m "Build: `date`"
+
+echo "\n3. Pushing source code"
+## Push code to "site" branch in target repository
+git push $REMOTE source
+
+echo "\n4. Pushing built website (--force)"
 ## git subtree deployment to gh-pages here
-## basically, just pushes the `_site` directory to the specified <remote>/<branch>
-git subtree push --prefix _site origin master
+## basically, push the `_site` directory to master
+git subtree split --squash --prefix _site -b build
+git push -f $REMOTE build:master
