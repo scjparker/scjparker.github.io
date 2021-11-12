9da57b4dfbf02485192d16b0d286607de708c447
Vivek Rai <mail@raivivek.in>
2021-11-12 13:24:18 -0500

    Build: Fri Nov 12 13:24:18 EST 2021

--- deploy.sh
+++ deploy.sh
@@ -12,21 +12,22 @@ read -n 1 -s -r -p "Press any key to continue"
 echo "\n0. Making sure dependencies are installed"
 bundle install
 
-echo "\n1. Building website.."
+echo "\n1. Pushing source code first"
+## Push code to "site" branch in target repository
+git push $REMOTE source
+
+echo "\n2. Building website.."
 bundle exec jekyll build
 
 echo "\n2. Commiting build.."
-
-## Add and commit build changes
-git add -A
-git commit -m "Build: `date`"
-
-echo "\n3. Pushing source code"
-## Push code to "site" branch in target repository
-git push $REMOTE source
 
 echo "\n4. Pushing built website (--force)"
 ## git subtree deployment to gh-pages here
 ## basically, push the `_site` directory to master
 git subtree split --squash --prefix _site -b build
+
+## Add and commit build changes
+git add -A
+git commit -m "Build: `date`"
+
 git push -f $REMOTE build:master
